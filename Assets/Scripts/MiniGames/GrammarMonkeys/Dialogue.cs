8bda0302ae97384599484fbe116bfe02b90b4ca7
Sjoekje28 <47067156+Sjoekje28@users.noreply.github.com>
2019-12-17 23:10:59 +0100

    Revert "More grammar monkeys"
    This reverts commit c51c0ccf41b030bbd188f1091e2aae91958f4c3c.

--- Assets/Scripts/MiniGames/GrammarMonkeys/Dialogue.cs
+++ Assets/Scripts/MiniGames/GrammarMonkeys/Dialogue.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class Dialogue
+public class Dialogue : MonoBehaviour
 {
-    public string name;
-    [TextArea(3,13)]
-    public string[] sentences;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
