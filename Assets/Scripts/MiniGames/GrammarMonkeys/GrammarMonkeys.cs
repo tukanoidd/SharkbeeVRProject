2aed45ac7781d59db26c05de6d5043e09cfd7eb4
Sjoekje28 <47067156+Sjoekje28@users.noreply.github.com>
2019-12-18 13:36:54 +0100

    more grammar minigame

---
+++ Assets/Scripts/MiniGames/GrammarMonkeys/GrammarMonkeys.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrammarMonkeys : MonoBehaviour
+{
+    private GrammarMonkey leftGrammarMonkey;
+    private GrammarMonkey rightGrammarMonkey;
+
+    public bool questionPresented;
+    
+    Argument argument = new Argument();
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
+
+class Argument
+{
+    public bool initialized = false;
+    public bool ended = false;
+    
+    public void Initialize( /*m1, m2*/)
+    {
+    }
+
+    public void NextText( /*m1, m2*/)
+    {
+    }
+
+    public void CheckArgumentEnd()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player/CharacterGrammarBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGrammarBehavior : MonoBehaviour
+{
+    private Character character;
+    
+    private bool inGrammarArea = false;
+    public bool grammarStarted = false;
+    public bool grammarDone = false;
+
+    [SerializeField] private GrammarMonkey grammarMonkey;
+
+    [SerializeField] private OVRInput.RawAxis1D rightGrammarMonkeyAnswerCheck;
+    [SerializeField] private OVRInput.RawAxis1D leftGrammarMonkeyAnswerCheck;
+
+    private GrammarMonkeys monkeys;
+    
+    void Start()
+    {
+        character = GetComponent<Character>();
+    }
+    
+    void Update()
+    {
+        if (!grammarDone)
+        {
+            CheckGrammarDistance();
+
+            if (inGrammarArea && !grammarStarted)
+            {
+                grammarStarted = true;
+            }
+
+            if (monkeys.questionPresented)
+            {
+                
+            }
+    }
+    
+    void CheckGrammarDistance()
+    {
+        float playerGrammarMonkeyDistance =
+            Vector3.Distance(transform.position, grammarMonkey.transform.position);
+
+        inGrammarArea = playerGrammarMonkeyDistance <= grammarMonkey.grammarAreaDistance;
+    }
+}
