9b88bfb51da083713a3e20de059f4f932177d78c
Vitalii Lukyanov <tukanoid@outlook.com>
2020-01-07 17:26:02 +0100

    some small changes b4 merge

--- Assets/Scripts/MiniGames/GrammarMonkeys/GrammarMonkey.cs
+++ Assets/Scripts/MiniGames/GrammarMonkeys/GrammarMonkey.cs
@@ -6,34 +6,17 @@ using UnityEngine;
 //In GrammarMonkey the dialogues are made
 public class GrammarMonkey : MonoBehaviour
 {
-    public float grammarAreaDistance = 6; // Why is this here and not in behavior
-    public float nearGrammarMonkeyDistance; //not used anywhere, yet?
-    
-    [TextArea]
-    public string[] argumentSentences;
-    public Answer[] answers;
-    [TextArea]
-    public string[] endSentences;
-    
-    
+    public float grammarAreaDistance = 6;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-}
 
-[Serializable] public class Answer
-{
-    [TextArea]
-    public string answer;
-    public bool right;
-    [TextArea]
-    public string explanation;
+    }
 }
